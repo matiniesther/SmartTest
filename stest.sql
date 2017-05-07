db71aeee6c28ce9667d88c81fabb8b2bf78850e0
CSC190 GitAccount <csc190Spr2017@gmail.com>
2017-05-07 00:53:27 -0400

    database implementation

---
+++ stest.sql
@@ -0,0 +1,32 @@
+# To run it:
+# mysql -u root -p < stest.sql
+# enter pwd: goodyear123!@#
+
+DROP DATABASE IF EXISTS stest_db;
+CREATE DATABASE stest_db;
+USE stest_db;
+CREATE TABLE tbl_users(
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	name VARCHAR(255),
+	role VARCHAR(255),
+	usename VARCHAR(255),
+	pword TEXT
+);
+INSERT INTO objects (name, role, username, pword) VALUES ("Jane Doe","Teacher", "janedoe@hofstra.edu" "INVALIDPWOED");
+CREATE TABLE tbl_test(
+	pinID INT PRIMARY KEY,
+	testname VARCHAR(255),
+	teachername VARCHAR(255),
+	testval TEXT
+);
+INSERT INTO objects (pinID,testname, teachername, testval) VALUES ("100","Java", "Jane Doe", "INVALIDTESTVAL");
+CREATE TABLE tbl_results(
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	pinID INT,
+	studentID INT,
+	resval TEXT
+);
+INSERT INTO objects (pinID, studentID,resval) VALUES ("23034", "701942994", "INVALIDRESVAL");
+
+
+
